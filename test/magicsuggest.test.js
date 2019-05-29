541034de3350777ad70955cee24aad6ab7ab94b8
jerry <aafulin@gmail.com>
2019-05-29 20:42:24 +0800

    add 2 test case

--- test/magicsuggest.test.js
+++ test/magicsuggest.test.js
@@ -14,4 +14,41 @@ describe('Magic Suggest Plugin Test',()=>{
         const ms = $('#city').magicSuggest({});
         expect(ms.getValue()).toEqual(["Tainan","Taipei", "Kaohsiung"]);
     });
+
+
+    it('change maxSelectionRenderer message when init',()=>{
+        document.body.innerHTML = `
+        <input id="city" />
+        `;
+        const ms = $('#city').magicSuggest({
+            maxSelection: 1,
+            maxSelectionRenderer:function() {
+            return 'I change the message you see';
+        }});
+        ms.setValue(['Tainan']);
+        expect(ms.helper.text()).toBe('I change the message you see');
+    });
+
+    it('provides the entries to fill up the combo box',()=>{
+        document.body.innerHTML = `
+        <input id="city" />
+        `;
+        const ms = $('#city').magicSuggest({
+            data: ['Tainan', 'Taipei', 'Kaohsiung']
+        });
+
+        let expected = [{
+            id: "Tainan",
+            name: "Tainan"
+        }, {
+            id: 'Taipei',
+            name: 'Taipei'
+        }, {
+            id: 'Kaohsiung',
+            name: 'Kaohsiung'
+        }];
+        
+        expect(ms.getData()).toEqual(expected);
+
+    });
 });
